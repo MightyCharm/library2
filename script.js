4708e80f51a5b0107a8000b442d0704b26c7f018
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2025-06-02 18:47:15 +0200

    feat: add functionality to create cards from user input

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // grab buttons
 const btnAddBook = document.querySelector(".btnAddBook");
-// const btnRead = document.querySelector(".btnRead");
-// const btnRemove = document.querySelector(".btnRemove");
+
 // modal window
 const modalWindow = document.querySelector("#modal");
 const btnCloseModal = document.querySelector("#btnCloseModal");
@@ -9,35 +8,29 @@ const btnSubmitModal = document.querySelector("#btnSubmitModal");
 const inputTitle = document.querySelector("#title");
 const inputAuthor = document.querySelector("#author");
 const inputPages = document.querySelector("#pages");
+const radioBtnFalse = document.querySelector("#radio-read-false");
+const radioBtnsRead = document.getElementsByName("read");
+
 // grab container for cards so cards can be appended later
 const cardContainer = document.querySelector("#card-container");
 
-
+// opens modal window
 btnAddBook.addEventListener( "click", () => {
   console.log("btnAddBook works");
   modalWindow.showModal();
 })
 
-// btnRead.addEventListener("click", () => {
-//   console.log("btnRead works");
-// })
-
-// btnRemove.addEventListener("click", () => {
-//   console.log("btnRemove works");
-// })
 
 btnCloseModal.addEventListener("click", () => {
   console.log("btnCloseModal");
   modalWindow.close();
 })
 
-btnSubmitModal.addEventListener("click", () => {
-  modalWindow.close();
-  inputTitle.value = "";
-  inputAuthor.value = "";
-  inputPages.value = "";
+btnSubmitModal.addEventListener("click", (event) => {
+  console.log("inside eventListener btnSubmitModal");
+  event.preventDefault();
+  main();
 })
-
 
 inputPages.addEventListener("input", (event) => {
   const NUMBER_MAX_LENGTH = 30;
@@ -71,12 +64,23 @@ function addBookToLibrary(obj) {
   myLibrary.push(obj);
 }
 
-const createBook = ( (title, author, pages, read) => {
-  const book = new Book(title, author, pages, read);
+const createBook = ( (obj) => {
+  const book = new Book(obj.title, obj.author, obj.pages, obj.read);
   addBookToLibrary(book);
 })
 
 //---------------------------------------------------------------------------------------------
+const clearCards = () => {
+  console.log("clearCards()");
+  // cardContainer.innerHTML = "";
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    console.log(card);
+  })
+  cards.forEach(card => card.remove());
+  
+}
+
 // create function that loops through array and displays books as cards
 const displayCards = () => {
   console.log("function createCards()");
@@ -161,22 +165,72 @@ const createCard = (book) => {
   })
 }
 
+const getUserInput = () => {
+  console.log("function getUserInput()");
+  const inputData = {
+    title: undefined,
+    author: undefined,
+    pages: undefined,
+    read: undefined
+  }
+  inputData.title = inputTitle.value;
+  inputData.author = inputAuthor.value;
+  inputData.pages = inputPages.value;
+  inputData.read = getRadioBtnValueModal();
+  
+  console.log(`date get from modal: title: ${inputData.title} - author: ${inputData.author} - pages: ${inputData.pages} - read: ${inputData.read};`)
+  console.log("validate input");
+  const checkInputTitle = inputData.title !== "";
+  const checkInputAuthor = inputData.author !== "";
+  const checkInputPages = inputData.pages !== "";
+  if(checkInputTitle && checkInputAuthor && checkInputPages) {
+    console.log("Everything was entered, return obj containing input");
+    return inputData;
+  }
+  if(checkInputTitle === false) {
+    console.log("Enter something into title input");
+  }
+  if(checkInputAuthor === false) {
+    console.log("Enter something into author input");
+  }
+  if(checkInputPages === false) {
+    console.log("Enter something into pages input");
+  }
+  return false;
+}
 
-// create 5 books
-createBook("Lord of the Rings 1", "J. R. R. Tolkien", 1000, true);
-createBook("Lord of the Rings 2", "J. R. R. Tolkien", 1200, true);
-createBook("Lord of the Rings 3", "J. R. R. Tolkien", 900, false);
-createBook("Harry Potter 1", "J. K. Rowling", 1900, true);
-createBook("1984", "Orwell", 600, true);
+// checks which radio button in modal window is selected, called from getUserInput();
+const getRadioBtnValueModal = () => {
+  for(let i=0; i< radioBtnsRead.length; i++) {
+    if (radioBtnsRead[i].checked) {
+      return radioBtnsRead[i].value;
+    }
+  }
+}
 
+const resetModal = () => {
+  console.log("function clearInputModal()");
+  inputTitle.value = "";
+  inputAuthor.value = "";
+  inputPages.value = "";
+  radioBtnFalse.checked = true;
+}
 
-// console.log(book1.info());
-// console.log(book2.info());
-// console.log(book3.info());
-// console.log(book4.info());
-// console.log(book5.info());
-
-
-displayCards();
-
+// main function should be the head of all functions because it was to
+// confusing...
+// main function called:
+// 1. from Submit Button Modal Window
+const main = () => {
+  console.log("main()");
+  const input = getUserInput();
+  console.log(`input: ${input} typeof: ${typeof input}`);
+  // if user entered data correct
+  if(input !== false) { 
+    resetModal();
+    modalWindow.close();
+    createBook(input);
+    clearCards();
+    displayCards();
+  }
+}
 //---------------------------------------------------------------------------------------------
