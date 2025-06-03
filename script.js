7f0c60a8cfa44d916449eafa13cfa827a8425636
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2025-06-03 17:41:33 +0200

    refactor: enhance main function to improve code clarity and maintainability

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // grab buttons
 const btnAddBook = document.querySelector(".btnAddBook");
-
 // modal window
 const modalWindow = document.querySelector("#modal");
 const btnCloseModal = document.querySelector("#btnCloseModal");
@@ -10,28 +9,29 @@ const inputAuthor = document.querySelector("#author");
 const inputPages = document.querySelector("#pages");
 const radioBtnFalse = document.querySelector("#radio-read-false");
 const radioBtnsRead = document.getElementsByName("read");
-
 // grab container for cards so cards can be appended later
 const cardContainer = document.querySelector("#card-container");
 
 // opens modal window
-btnAddBook.addEventListener( "click", () => {
-  console.log("btnAddBook works");
-  modalWindow.showModal();
+btnAddBook.addEventListener( "click", (event) => {
+  console.log("btnAddBook.addEventListener()");
+  main(event);
 })
 
-btnCloseModal.addEventListener("click", () => {
-  console.log("btnCloseModal");
-  modalWindow.close();
+btnCloseModal.addEventListener("click", (event) => {
+  console.log("btnCloseModal.addEventListener()");
+  main(event);
 })
 
 btnSubmitModal.addEventListener("click", (event) => {
-  console.log("inside eventListener btnSubmitModal");
+  console.log("btnSubmitModal.addEventListener()");
   event.preventDefault();
-  main();
+  main(event)
 })
 
+// prevents that number input doesn't get to long
 inputPages.addEventListener("input", (event) => {
+  console.log("inputPages.addEventListener()");
   const NUMBER_MAX_LENGTH = 30;
   // 1. check that if user enters page number of book, this number isn't to long
   if(event.target.value.length > NUMBER_MAX_LENGTH) {
@@ -44,7 +44,7 @@ const myLibrary = [];
 
 // constructor function for creating book objects
 function Book(title, author, pages, read) {
-  console.log("Inside constructor");
+  console.log("function Book(constructor)");
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
@@ -60,30 +60,26 @@ function Book(title, author, pages, read) {
 
 // function adds book objects to library
 function addBookToLibrary(obj) {
+  console.log("addBookToLibrary()")
   myLibrary.push(obj);
 }
 
 const createBook = ( (obj) => {
+  console.log("createBook()")
   const book = new Book(obj.title, obj.author, obj.pages, obj.read);
   addBookToLibrary(book);
 })
 
-//---------------------------------------------------------------------------------------------
-const clearCards = () => {
-  console.log("clearCards()");
-  // cardContainer.innerHTML = "";
-  const cards = document.querySelectorAll(".card");
-  cards.forEach((card) => {
-    console.log(card);
-  })
-  cards.forEach(card => card.remove());
-  
-}
 
 // create function that loops through array and displays books as cards
 // called from main/removeCards
 const displayCards = () => {
-  console.log("function createCards()");
+  console.log("displayCards()");
+  // first clear GUI from all cards
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    card.remove();
+  })
   // iterate through array and call createCard for every book object to create a card
   myLibrary.forEach( (book) => {
     // call function to create a card
@@ -93,13 +89,9 @@ const displayCards = () => {
 
 // remove card from library array and from gui
 const removeCardFromDataGUI = (id) => {
-  console.log("removeCard()");
-  console.log(id);
-  console.log("length myLibrary: " + myLibrary.length);
+  console.log("removeCardFromDataGUI()");
   // remove card from dataset
   for(let i = 0; i < myLibrary.length; i++) {
-    console.log("Inside for loop");
-    console.log(myLibrary[i].id + " : " + id);
     if(myLibrary[i].id === id) {
       myLibrary.splice(i, 1);
     }
@@ -110,7 +102,7 @@ const removeCardFromDataGUI = (id) => {
 
 // called from function displayCards to create a card for every book object
 const createCard = (book) => {
-  console.log("function createCard()");
+  console.log("createCard()");
   // create card
   //div class="card" main container
   const divCard = document.createElement("div");
@@ -152,7 +144,7 @@ const createCard = (book) => {
   btnRead.textContent = "READ";
   btnRead.classList.add("btn");
   btnRead.classList.add("btnRead");
-
+  btnRead.dataset.action = "readBook";
   divRead.appendChild(pRead);
   divRead.appendChild(btnRead);
 
@@ -161,6 +153,7 @@ const createCard = (book) => {
   btnRemove.textContent = "REMOVE";
   btnRemove.classList.add("btn");
   btnRemove.classList.add("btnRemove");
+  btnRemove.dataset.action = "removeBook";
 
   // append everything to divCard
   divCard.appendChild(divTitle);
@@ -172,20 +165,19 @@ const createCard = (book) => {
   cardContainer.appendChild(divCard);
 
   // append eventlistener to button
-  btnRead.addEventListener("click", () => {
-    console.log("btnRead works");
+  btnRead.addEventListener("click", (event) => {
+    console.log("btnRead.addEventListener()");
+    main(event);
   })
 
-  btnRemove.addEventListener("click", (e) => {
-    const cardId = e.target.closest(".card").id;
-    removeCardFromDataGUI(cardId);
-    // console.log(cardId);
-    
+  btnRemove.addEventListener("click", (event) => {
+    console.log("btnRemove.addEventListener()");
+    main(event);
   })
 }
 
 const getUserInput = () => {
-  console.log("function getUserInput()");
+  console.log("getUserInput()");
   const inputData = {
     title: undefined,
     author: undefined,
@@ -196,7 +188,7 @@ const getUserInput = () => {
   inputData.author = inputAuthor.value;
   inputData.pages = inputPages.value;
   inputData.read = getRadioBtnValueModal();
-  console.log(`date get from modal: title: ${inputData.title} - author: ${inputData.author} - pages: ${inputData.pages} - read: ${inputData.read};`)
+  // console.log(`date get from modal: title: ${inputData.title} - author: ${inputData.author} - pages: ${inputData.pages} - read: ${inputData.read};`)
  
   const checkInputTitle = inputData.title !== "";
   const checkInputAuthor = inputData.author !== "";
@@ -218,6 +210,7 @@ const getUserInput = () => {
 
 // checks which radio button in modal window is selected, called from getUserInput();
 const getRadioBtnValueModal = () => {
+  console.log("getRadioBtnValueModal()");
   for(let i=0; i< radioBtnsRead.length; i++) {
     if (radioBtnsRead[i].checked) {
       return radioBtnsRead[i].value;
@@ -226,28 +219,43 @@ const getRadioBtnValueModal = () => {
 }
 
 const resetModal = () => {
-  console.log("function clearInputModal()");
+  console.log("resetModal()");
   inputTitle.value = "";
   inputAuthor.value = "";
   inputPages.value = "";
   radioBtnFalse.checked = true;
 }
 
-
-// main function should be the head of all functions because it was to
-// confusing...
-// main function called:
-// 1. from Submit Button Modal Window
-const main = () => {
-  console.log("main()");
-  const input = getUserInput();
-  // if user entered data correct
-  if(input !== false) { 
+// gets called from all eventlistener
+const main = (event) => {
+  console.log("main function()");
+  const action = event.target.getAttribute("data-action");
+  console.log(action);
+  if(action === "newBook") {
+    console.log("newBook was clicked");
+    modalWindow.showModal();
+  }
+  else if(action === "submitModal") {
+    console.log("submitModal was clicked");
+    const input = getUserInput();
+    if(input !== false) { 
+      resetModal();
+      modalWindow.close();
+      createBook(input);
+      displayCards();
+    }
+  }
+  else if(action === "closeModal") {
+    console.log("closeModal was clicked");
     resetModal();
     modalWindow.close();
-    createBook(input);
-    clearCards();
-    displayCards();
+  }
+  else if(action === "readBook") {
+    console.log("readBook was clicked");
+  }
+  else if(action === "removeBook") {
+    console.log("removeBook was clicked");
+    const cardId = event.target.closest(".card").id;
+    removeCardFromDataGUI(cardId);
   }
 }
-//---------------------------------------------------------------------------------------------
